4acafcab875ab1791861ce2792bbda7eebf103a6
Angel Gomez <tenshigomez@gmail.com>
2025-12-12 01:15:06 -0300

    chore: insert_after implemented

--- HelloWorld.c
+++ HelloWorld.c
@@ -36,12 +36,27 @@ void insert_tail(struct Node *node, struct Node *original, struct DoubleLinkedLi
 			node->next = NULL;
 			dbl->lastNode = node;
 		}
-	  original = original->next;
+		original = original->next;
 	}
 }
-void insert_after(struct Node *node, struct Node *original) {
+void insert_after(struct Node *target_node, char *value, struct Node *current_node) {
+	struct Node *new_node = malloc(sizeof(struct Node));
+	new_node->value = value;
+	while(current_node) {
+		if(current_node == target_node) {
+			new_node->prev = current_node;
+			new_node->next = current_node->next;
+			if(current_node->next) {
+				current_node->next->prev = new_node;
+			}
+			current_node->next = new_node;
+			break;
+		}
+		current_node = current_node->next;
+	}
 }
-void insert_before(struct Node *node, struct Node *original) {
+
+void insert_before(struct Node *target_node, char *value, struct Node *original) {
 }
 
 
@@ -80,11 +95,11 @@ int main() {
 	current_node = DBL->firstNode;
 
 
-	while (current_node) {
-		printf("Value of current node is %c\n", *current_node->value);
-		current_node = current_node->next;
-	};
-
+	/*	while (current_node) {
+			printf("Value of current node is %c\n", *current_node->value);
+			current_node = current_node->next;
+			};
+			*/
 
 	/* 
 		 Search Test
@@ -125,6 +140,22 @@ int main() {
 		 printf("new tail addrs %p\n", DBL->lastNode);
 		 printf("New Tail value is %c\n", *DBL->lastNode->value);
 		 */
+
+	// Test Insert After
+	char *new_string = malloc(2);
+	new_string[0] = '9';
+	new_string[1] = '\0'; 
+	printf("Second node value before insertion %c\n", *DBL->firstNode->next->value);
+	printf("Third node value before insertion %c\n", *DBL->firstNode->next->next->value);
+	insert_after(DBL->firstNode->next,new_string, DBL->firstNode);
+	printf("Second node value after insertion %c\n", *DBL->firstNode->next->value);
+	printf("Third node value after insertion %c\n", *DBL->firstNode->next->next->value);
+
+	struct Node *displayNode = DBL->firstNode;
+	while (displayNode) {
+		printf("Value of current node is %c\n", *displayNode->value);
+		displayNode = displayNode->next;
+	};
 
 	return 0;
 }
