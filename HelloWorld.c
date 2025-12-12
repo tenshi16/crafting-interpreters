088fa44f5213d86ce4231475cdea647c5dd8dbf1
Angel Gomez <tenshigomez@gmail.com>
2025-12-11 22:46:43 -0300

    chore: updating value to pointer to char

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,7 +4,7 @@
 struct Node {
 	struct Node *prev;
 	struct Node *next;
-	int value;
+	char *value;
 };
 
 struct DoubleLinkedList {
@@ -12,10 +12,9 @@ struct DoubleLinkedList {
 	struct Node *lastNode;
 };
 
-struct Node *search_node(int val, struct Node *target) {
-	struct Node *aux = target;
+struct Node *search_node(char val, struct Node *target) {
 	while(target) {
-		if(target->value == val) {
+		if(*target->value == val) {
 			printf("Node found, exiting search \n");
 			return target;
 		}
@@ -24,16 +23,20 @@ struct Node *search_node(int val, struct Node *target) {
 }
 
 
+
+
 int main() {
 	struct DoubleLinkedList *DBL = malloc(sizeof(struct DoubleLinkedList));
 
 	struct Node *current_node = malloc(sizeof(struct Node));
 	for(int i = 0; i<4; i++) {
-		printf("i value = %d\n", i);
+		char *as_string = malloc(2);
+		as_string[0] = i + '0';
+		as_string[1] = '\0';
 		struct Node *next_node = malloc(sizeof(struct Node));
 		if(i == 0) {
 			// Add the first node
-			current_node->value = i;
+			current_node->value = as_string;
 			current_node->prev = NULL;
 			current_node->next = next_node;
 			DBL->firstNode = current_node;
@@ -42,13 +45,12 @@ int main() {
 		}
 		if(i == 3) {
 			// Add the last node;
-			current_node->value = i;
+			current_node->value = as_string;
 			current_node->next = NULL;
 			DBL->lastNode = current_node;
-			printf("Memory Add of lastNode %p\n", current_node);
 			break;
 		} 
-		current_node->value = i;
+		current_node->value = as_string;
 		current_node->next = next_node; 
 		next_node->prev = current_node;
 		current_node = next_node;
@@ -56,14 +58,16 @@ int main() {
 
 	current_node = DBL->firstNode;
 
+
 	while (current_node) {
-		printf("Value of current node is %d\n", current_node->value);
+		printf("Value of current node is %c\n", *current_node->value);
 		current_node = current_node->next;
 	};
 	
 
 	printf("Search of node #3 starting\n");
-	struct Node *searched = search_node(3, DBL->firstNode);
+	char to_search = '3';
+	struct Node *searched = search_node(to_search, DBL->firstNode);
 
 	if(searched == DBL->firstNode) {
 		printf("First Node found\n");
@@ -71,7 +75,7 @@ int main() {
 	else if(searched == DBL->lastNode) {
 		printf("Last Node found\n");
 	}
-	printf("Memory Add of the found last address %p\n", searched);
-	printf("Result Node value = %d\n", searched->value);
+	printf("Result Node value = %c\n", *searched->value);
+
 	return 0;
 }
